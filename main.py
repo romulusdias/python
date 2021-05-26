53efd8baa8724c1e129b10ad7e7e7013e412575b
romulusdias <romulusdias@yahoo.com>
2021-05-27 01:19:39 +0530

    main file added

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Romulus')
+
+for num in range(2, 100):
+        if num % 2 == 0:
+            print("Found an even number", num)
+            continue
+            print("Found an odd number", num)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
